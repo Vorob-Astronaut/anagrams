a914b356cd25fb76d46b980164bad90df581f458
Oleg Leontiev <oleg@c1r.com>
2015-09-07 13:44:25 +0700

    refactoring in progress

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -8,18 +8,7 @@ class CountriesController < ApplicationController
 
   # GET /countrytitles/1
   def show
-    @titles = CountryTitle.find_by_title_id(params[:id])
-     @connection = ActiveRecord::Base.establish_connection(
-            :adapter => "mysql2",
-            :host => "localhost",
-            :database => "butaca_dev",
-            :username => "root",
-            :password => "1234"
-   )
-
-
-     @countries = Country.where('active' == '["1"]')
-      @genres = Genre.all
+    @country = Country.find(params[:id])
   end
 
 
--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -1,25 +1,11 @@
 class GenreController < ApplicationController
 
-
-  # GET /genretitles
    def index
      @genres = Genre.all
    end
 
-  # GET /genretitles/1
   def show
-     @connection = ActiveRecord::Base.establish_connection(
-            :adapter => "mysql2",
-            :host => "localhost",
-            :database => "butaca_dev",
-            :username => "root",
-            :password => "1234"
-   )
-
-     @titles = GenreTitle.all
-     @genres = Genre.find(params[:id])
-     @countries = Country.where('active' == '["1"]')
-
+     @genre = Genre.find(params[:id])
   end
 
 
