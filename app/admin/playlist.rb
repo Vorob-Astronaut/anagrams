2ee587fb78236f392125ee7b0b77905cc96fe4d5
Oleg Leontiev <oleg@c1r.com>
2015-09-13 12:45:28 +0700

    fix adding playlist

--- app/admin/playlist.rb
+++ app/admin/playlist.rb
@@ -17,6 +17,14 @@ permit_params :collection_id , :date_time , :title_id
 filter :title_film_title_cont, as: :select, collection: -> {Title.all.collect(&:film_title)}, label: "Title"
 filter :collection_collection_name_eq, as: :select, collection: -> {Collection.all.collect(&:collection_name)}, label: "Collection"
 
+form do |f|
+  inputs 'Details' do
+    f.input :collection, as: :select, collection: Collection.all.map{|c| [c.collection_name, c.id]}, label: "Collection"
+    f.input :title, as: :select, collection: Title.all.map{|t| [t.film_title, t.id]}, label: "Title"
+    f.actions
+  end
+end
+
 index do
   column :title do |col|
     col.title.film_title if col.title
