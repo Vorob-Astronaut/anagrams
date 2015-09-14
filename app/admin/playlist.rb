d37e886b03911f7f6c891fc0e06d6b605a2d0c32
Oleg Leontiev <oleg@c1r.com>
2015-09-14 14:24:05 +0700

    fix bugs

--- app/admin/playlist.rb
+++ app/admin/playlist.rb
@@ -12,27 +12,41 @@ ActiveAdmin.register Playlist do
 #   permitted << :other if resource.something?
 #   permitted
 # end
-permit_params :collection_id , :date_time , :title_id
+  permit_params :collection_id , :date_time , :title_id
 
-filter :title_film_title_cont, as: :select, collection: -> {Title.all.collect(&:film_title)}, label: "Title"
-filter :collection_collection_name_eq, as: :select, collection: -> {Collection.all.collect(&:collection_name)}, label: "Collection"
+  filter :title_film_title_cont, as: :select, collection: -> {Title.all.collect(&:film_title)}, label: "Title"
+  filter :collection_collection_name_eq, as: :select, collection: -> {CollectionTranslation.where(locale: :es).collect(&:collection_name)}, label: "Collection"
 
-form do |f|
-  inputs 'Details' do
-    f.input :collection, as: :select, collection: Collection.all.map{|c| [c.collection_name, c.id]}, label: "Collection"
-    f.input :title, as: :select, collection: Title.all.map{|t| [t.film_title, t.id]}, label: "Title"
-    f.actions
+  form do |f|
+    inputs 'Details' do
+      f.input :collection, as: :select, collection: Collection.all.map{|c| [c.collection_name, c.id]}, label: "Collection"
+      f.input :title, as: :select, collection: Title.all.map{|t| [t.film_title, t.id]}, label: "Title"
+      f.actions
+    end
   end
-end
 
-index do
-  column :title do |col|
-    col.title.film_title if col.title
+  index do
+    column :id
+    column :title do |col|
+      col.title.film_title if col.title
+    end
+    column :collection do |col|
+      col.collection.collection_name if col.collection
+    end
+    actions
   end
-  column :collection do |col|
-    col.collection.collection_name if col.collection
+
+  show do
+    attributes_table do
+      row :id
+      row :collection do |c|
+        c.collection.collection_name
+      end
+      row :title do |t|
+        t.title.film_title
+      end
+    end
   end
-  actions
-end
+
 
 end
