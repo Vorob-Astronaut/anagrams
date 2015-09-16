dd2be83ff928071c9cdf3af9b7be84bd8752b1d8
Oleg Leontiev <oleg@c1r.com>
2015-09-16 09:57:38 +0700

    hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
     @country_list = Country.where(is_active?: true)
     @collection_list = Collection.all
     @q = Title.ransack params[:q]
-    @last_notification = current_user.user_log_activities.last(5).collect(&:message)
+    @last_notification = current_user.user_log_activities.last(5).collect(&:message) if current_user
   end
 
   def set_locale
