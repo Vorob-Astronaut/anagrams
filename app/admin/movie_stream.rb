8a6e9508e77a26f53e04427416401862e047ce17
Oleg Leontiev <oleg@c1r.com>
2015-09-23 16:42:40 +0700

    fix backend admin

--- app/admin/movie_stream.rb
+++ app/admin/movie_stream.rb
@@ -1,5 +1,19 @@
 ActiveAdmin.register MovieStream do
 
+
+  form do |f|
+    inputs 'Details' do
+      f.input :title, collection: Title.all.map{|t| [t.film_title, t.id]}
+      f.input :typel
+      f.input :link
+      f.input :link_type
+      f.input :price
+      f.input :logo
+      f.actions
+    end
+  end
+
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
