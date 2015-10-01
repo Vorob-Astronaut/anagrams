1cced1437aab92d0b86a7b7ec8adf89fc3249081
Oleg Leontiev <oleg@c1r.com>
2015-10-01 16:16:57 +0700

    better notifications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,11 @@ class UserMailer < ApplicationMailer
 
   def daily_mail(user)
      @user = user
-     @notifications = @user.user_log_activities.where("created_at >= ?", Time.zone.now.beginning_of_day)
+     if @user.notification_preferences == 'daily' then
+       @notifications = @user.user_log_activities.where("created_at >= ?", Time.zone.now.beginning_of_day)
+     elsif @user.notification_preferences == 'weekly'
+       @notifications = @user.user_log_activities.where("created_at >= ?", Time.zone.now.beginning_of_week-1.week)
+     end
      mail(to: @user.email, subject: 'Your notifications from butaca')
   end
 
