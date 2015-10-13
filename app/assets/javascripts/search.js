96103e99a8c7ce2eaa4a89f0505e0ecb74fd0330
Amilcar <ajose.erazo@gmail.com>
2015-10-13 15:13:00 -0430

    Adding custom scrollbar

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,44 +1,59 @@
-  $(function() {
-    $("#add_a_film").click(function() {
-      $("#add_film_form").toggle();
-    });
-
-
-    $("#search_field").autocomplete({
-      source: function (request, response) {
-          $.ajax({
-              url: "/ac.json",
-              dataType: "json",
-              data: request,
-              success: function (data) {
-                  if (!data.length) { // expect [] or ""
-                      response(["We don't have that film on the site yet. Would you like us to add it?"])
-                  } else {
-                      console.log(data)
-                      response(data);
-                  }
-              }
-          });
-      },
-      minLength: 1
-    });
-    $("#q_film_title_cont").autocomplete({
-      source: "/ac.json",
-      minLength: 1
-    });
-
-    // This is for handle the modal for titles
-    $('.product > a').click( function(event){
-      var newUrl = $(this).attr('href');
-
-      window.history.pushState({}, null, newUrl);
-    });
-
-    $('#modalTitle .close').click(function (e) {
-      window.history.back();
-    });
-    //------------------------------------//
+$(function() {
+  $("#add_a_film").click(function() {
+    $("#add_film_form").toggle();
   });
+
+
+  $("#search_field").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "/ac.json",
+            dataType: "json",
+            data: request,
+            success: function (data) {
+                if (!data.length) { // expect [] or ""
+                    response(["We don't have that film on the site yet. Would you like us to add it?"])
+                } else {
+                    console.log(data)
+                    response(data);
+                }
+            }
+        });
+    },
+    minLength: 1
+  });
+  $("#q_film_title_cont").autocomplete({
+    source: "/ac.json",
+    minLength: 1
+  });
+
+  // This is for handle the modal for titles
+  $('.product > a').click( function(event){
+    var newUrl = $(this).attr('href');
+
+    window.history.pushState({}, null, newUrl);
+  });
+
+  $('#modalTitle .close').click(function (e) {
+    window.history.back();
+  });
+  //------------------------------------//
+
+  setSidebarHeight();
+
+  $(window).resize( function(){
+    setSidebarHeight();
+  })
+});
+
+function setSidebarHeight(){
+  var height = $(window).height();
+
+  $('.sidebarr').css({
+    height: height - (90 + 55),
+    maxHeight: height - (90 + 55)
+  });
+}
 
 
 $('#modalTitle').on('hidden.bs.modal', function (e) {
