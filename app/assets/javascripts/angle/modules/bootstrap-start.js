3b34d4f2c39a0306415d423add66af864f0cdfe4
Amilcar <ajose.erazo@gmail.com>
2015-10-16 21:01:17 -0430

    Fixing modal width

--- app/assets/javascripts/angle/modules/bootstrap-start.js
+++ app/assets/javascripts/angle/modules/bootstrap-start.js
@@ -1,24 +1,24 @@
 // Start Bootstrap JS
-// ----------------------------------- 
+// -----------------------------------
 
 (function(window, document, $, undefined){
 
   $(function(){
 
     // POPOVER
-    // ----------------------------------- 
+    // -----------------------------------
 
     $('[data-toggle="popover"]').popover();
 
     // TOOLTIP
-    // ----------------------------------- 
+    // -----------------------------------
 
     $('[data-toggle="tooltip"]').tooltip({
       container: 'body'
     });
 
     // DROPDOWN INPUTS
-    // ----------------------------------- 
+    // -----------------------------------
     $('.dropdown input').on('click focus', function(event){
       event.stopPropagation();
     });
