d0eeb1c891551055ef94edee33105acc68b28089
Egor Vorobiev <vorobievegoralexandrovich@gmail.com>
2015-10-28 15:42:46 +0300

    Remove http basic auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   add_breadcrumb "home", :root_path
   skip_before_filter :authenticate_admin_user!, only: [:welcome]
-  http_basic_authenticate_with name: "admin", password: "passwd", only: [:welcome]
 
   def index
     @collections = Collection.all
